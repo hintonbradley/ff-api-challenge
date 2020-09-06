e4d836293e720c33d852cc3b0bea88ca7d2003ab
hintonbradley <hintonbradley@gmail.com>
2020-09-05 22:38:34 -0700

    small styling changes

--- src/components/SuccessApiDetails.js
+++ src/components/SuccessApiDetails.js
@@ -5,7 +5,7 @@ const SuccessApiDetails = function (props) {
     return (
         <li className="flex-row apihealth-success">
             <p className="api-name">{props.name.charAt(0).toUpperCase() + props.name.slice(1)}</p>
-            <p className="success api-status">Success</p>
+            <p className="success api-status">OK</p>
             <p className="api-message">{props.api.message}</p>
             <p className="api-hostname">{props.api.hostname}</p>
             <p className="api-time">{props.time}</p>
