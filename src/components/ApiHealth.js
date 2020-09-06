14472896f87a6874920af172c995561f07c00fa0
hintonbradley <hintonbradley@gmail.com>
2020-09-05 17:43:25 -0700

    adding ability to fetch and display data

--- src/components/ApiHealth.js
+++ src/components/ApiHealth.js
@@ -3,42 +3,79 @@ import api from '../api/api';
 
 class ApiHealth extends React.Component {
     state = {
-        apiList : ['accounts', 'assets'],
-        accounts: {
-            success: null,
-            message: null,
-            hostname: null,
-            time: null
-        },
-        assets: {
-            success: null,
-            message: null,
-            hostname: null,
-            time: null
-        }
+        checkApiTimer: 15,
+        apiList : ['accounts', 'assets','customers','datapoints','devices','documents','forms','invites','media','messages','namespaces','orders','patients','relationships','rules','templates','users','workflows'],
+        accounts: {success:null},
+        assets: {success:null},
+        customers: {success:null},
+        datapoints: {success:null},
+        devices: {success:null},
+        documents: {success:null},
+        forms: {success:null},
+        invites: {success:null},
+        media: {success:null},
+        messages: {success:null},
+        namespaces: {success:null},
+        orders: {success:null},
+        patients: {success:null},
+        relationships: {success:null},
+        rules: {success:null},
+        templates: {success:null},
+        users: {success:null},
+        workflows: {success:null}
     }
 
     componentDidMount () {
-        setInterval( this.getApiHealth, 15000);
+        this.state.apiList.forEach(name => {
+            this.getApiHealth(name)
+        })
     }
 
-
-    getApiHealth = async (name) => {
-        // testing getting health data from a particular api from a list of api names and then updating state
-        const resp = await api.get(`https://api.factoryfour.com/${this.state.apiList[0]}/health/status`)
-        this.setState({ accounts: resp.data})
-        console.log('new state is', this.state)
+    getApiHealth (name) {
+        setInterval(async () => {
+            try {
+                const resp = await api.get(`https://api.factoryfour.com/${name}/health/status`) 
+                this.setState({ [name]: resp.data})
+            } catch (e) {
+                this.setState({[name]: {success: false}})
+            }
+        }, this.state.checkApiTimer*1000);
     }
 
-
+    renderResponses () {
+        return this.state.apiList.map( (name, i) => {
+            if(this.state[name].success) {
+                return (
+                    <div key={this.state[name].hostname}>
+                        <h1>{name}</h1>
+                        <p>Success: {this.state[name].success}</p>
+                        <p>Message: {this.state[name].message}</p>
+                        <p>Hostname: {this.state[name].hostname}</p>
+                        <p>Time: {this.state[name].time}</p>
+                    </div>
+                )
+            } else if(this.state[name].success === null){
+                return (
+                    <div key={i}>
+                        <h1>{name}</h1>
+                        <p>Loading...</p>
+                    </div>
+                )
+            } else {
+                return (
+                    <div key={i}>
+                        <h1>{name}</h1>
+                        <p>Error 503!</p>
+                    </div>
+                )
+            }
+        })
+    }
 
     render () {
         return (
             <div>
-                <p>Success: {this.state.accounts.success}</p>
-                <p>Message: {this.state.accounts.message}</p>
-                <p>Hostname: {this.state.accounts.hostname}</p>
-                <p>Time: {this.state.accounts.time}</p>
+                <div>{this.renderResponses()}</div>
             </div>
         )
     }
